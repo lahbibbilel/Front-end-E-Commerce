b610f1241a62fd7794d9e41f361cd7c4b863c040
lahbibbilel <lahbibbilel07@gmail.com>
2023-02-19 23:40:41 +0100

    Api-get

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./public/home/home.component";
+import {ProduitComponent} from "./public/produit/produit.component";
+import {ContactComponent} from "./public/contact/contact.component";
+import {DashboardPublicComponent} from "./dashboard-public/dashboard-public.component";
+import {ErrorComponent} from "./errors/error/error.component";
+import {AuthGuard} from "./guard/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',component: DashboardPublicComponent, children : [
+  { path: '',redirectTo: 'home', pathMatch: "full"},
+  { path: 'home',component: HomeComponent},
+  { path: 'produit',component: ProduitComponent },
+  { path: 'contact',component: ContactComponent},
+    ]
+  },
+  {path : 'admin',loadChildren:() =>import('./admin/admin.module').then(m=>m.AdminModule),canActivate:[AuthGuard]},
+  {path : 'auth',loadChildren:() =>import('./auth/auth.module').then(m=>m.AuthModule)},
+  { path: '**',component: ErrorComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicComponent } from './public/public.component';
+import { HomeComponent } from './public/home/home.component';
+import { ProduitComponent } from './public/produit/produit.component';
+import { ContactComponent } from './public/contact/contact.component';
+import { DashboardPublicComponent } from './dashboard-public/dashboard-public.component';
+import { HeaderComponent } from './header/header.component';
+import { ErrorComponent } from './errors/error/error.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TokenInterceptorProvider} from "./interceptor/token.interceptor";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PublicComponent,
+        HomeComponent,
+        ProduitComponent,
+        ContactComponent,
+        DashboardPublicComponent,
+        HeaderComponent,
+        ErrorComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule
+    ],
+  //activer interceptor
+    providers: [
+      TokenInterceptorProvider
+    ],
+    exports: [
+        HeaderComponent
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
